9258819dc51c46b41aa6d17b516c79fdc7527921
Diego Leite <diego.pires.leite@gmail.com>
2020-12-08 22:24:52 -0300

    Other Entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,25 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department(){}
+
+        public Department(int id, string name, ICollection<Seller> sellers)
+        {
+            Id = id;
+            Name = name;
+            Sellers = sellers;
+        }
+
+        public void addSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double totalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.totalSales(initial, final));
+        }
     }
 }
