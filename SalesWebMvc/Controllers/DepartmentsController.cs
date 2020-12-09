db491cbcba178f72d4195199ac16a8753742c9e9
Diego Leite <diego.pires.leite@gmail.com>
2020-12-08 21:39:59 -0300

    MySQL adaptation and first migration

--- SalesWebMvc/Controllers/DepartmentsController.cs
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -22,7 +22,7 @@ namespace SalesWebMvc.Controllers
         // GET: Departments
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Departments.ToListAsync());
+            return View(await _context.Department.ToListAsync());
         }
 
         // GET: Departments/Details/5
@@ -33,7 +33,7 @@ namespace SalesWebMvc.Controllers
                 return NotFound();
             }
 
-            var departments = await _context.Departments
+            var departments = await _context.Department
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (departments == null)
             {
@@ -54,7 +54,7 @@ namespace SalesWebMvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Departments departments)
+        public async Task<IActionResult> Create([Bind("Id,Name")] Department departments)
         {
             if (ModelState.IsValid)
             {
@@ -73,7 +73,7 @@ namespace SalesWebMvc.Controllers
                 return NotFound();
             }
 
-            var departments = await _context.Departments.FindAsync(id);
+            var departments = await _context.Department.FindAsync(id);
             if (departments == null)
             {
                 return NotFound();
@@ -86,7 +86,7 @@ namespace SalesWebMvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Departments departments)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Department departments)
         {
             if (id != departments.Id)
             {
@@ -124,7 +124,7 @@ namespace SalesWebMvc.Controllers
                 return NotFound();
             }
 
-            var departments = await _context.Departments
+            var departments = await _context.Department
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (departments == null)
             {
@@ -139,15 +139,15 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var departments = await _context.Departments.FindAsync(id);
-            _context.Departments.Remove(departments);
+            var departments = await _context.Department.FindAsync(id);
+            _context.Department.Remove(departments);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool DepartmentsExists(int id)
         {
-            return _context.Departments.Any(e => e.Id == id);
+            return _context.Department.Any(e => e.Id == id);
         }
     }
 }
--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -14,6 +14,6 @@ namespace SalesWebMvc.Data
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Departments> Departments { get; set; }
+        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
     }
 }
